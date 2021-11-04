5d9040be8e8f11f3cb844fb8f7faeb97f94c9503
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-11-04 16:40:17 -0300

    O form está sendo controlado pelo estado do react

--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Form from '../components/Form'
-import Header from '../components/Header'
+import Form from '../components/Form/Form'
+import Header from '../components/Header/Header'
 
 export default function Login() {
   return (
